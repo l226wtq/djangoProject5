8e4a65729c266a922b2d1fc9dc0c1a5f87d313e4
lyy <857709855@qq.com>
2022-12-28 10:55:02 +0800

    删除接口 全屏功能优化

--- frontend/book_view_web/src/components/comicTabComponents/thumbnailView.vue
+++ frontend/book_view_web/src/components/comicTabComponents/thumbnailView.vue
@@ -13,7 +13,7 @@
           <p>{{ allBooksDataSource[(row_num - 1) * coverColNum + col_num - 1].title }}</p>
           <el-image
               :src="`http://172.17.18.115:8089/static/covers/${allBooksDataSource[(row_num - 1) * coverColNum + col_num - 1]?.id}.webp`"
-              @click="clcikThumbnail(allBooksDataSource[(row_num - 1) * coverColNum + col_num - 1]?.id)"></el-image>
+              @click="clcikThumbnail(allBooksDataSource[(row_num - 1) * coverColNum + col_num - 1].id)"></el-image>
           <el-row>
             <el-col :span="12">
               <el-input
@@ -67,6 +67,7 @@
                 title="确定要删除吗?"
                 confirm-button-text="确认"
                 cancel-button-text="手滑了"
+                @confirm="clcikDeleteButton(allBooksDataSource[(row_num - 1) * coverColNum + col_num - 1].id)"
             >
               <template #reference>
                 <el-button type="danger">删除</el-button>
@@ -79,10 +80,17 @@
         <!--          :src="`http://172.17.18.115:8089/static/covers/${allBooksDataSource[(row_num-1)*coverColNum+col_num-1]?.id}.webp`"></el-image>-->
       </el-col>
     </el-row>
+    <el-row>
+      <el-pagination background layout="prev, pager, next" :total="total" v-model:current-page="current_page"
+                     @current-change="pageChange"/>
+    </el-row>
   </div>
-  <div v-show="this.fullScreenViewMode">
+  <div v-if="this.fullScreenViewMode">
     <el-row>
       <el-button @click="clcikExitButton">退出全屏</el-button>
+      <el-button @click="clickPreviousPic">上一页</el-button>
+      <el-button @click="clickNextPic">下一页</el-button>
+      <el-button>单/双页切换</el-button>
       <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
         打开抽屉
       </el-button>
@@ -93,9 +101,10 @@
       >
         <span>Hi, there!</span>
       </el-drawer>
+      <p>currentPage:{{ this.page }} -- bookLength:{{ this.CurrentBookLength }}</p>
     </el-row>
     <el-row>
-      <el-image :src="viewPicUrl" @click="clickNextPic" fit="contain"></el-image>
+      <el-image :src="viewPicUrl" @click="clickNextPic" fit="contain" usemap="#planetmap"></el-image>
     </el-row>
   </div>
 
@@ -103,6 +112,7 @@
 
 <script>
 import axios from "axios";
+import {ElMessage} from 'element-plus'
 
 export default {
   name: "thumbnailView",
@@ -110,17 +120,25 @@ export default {
     return {
       allBooksDataSource: [],
       currentLibPage: 1,
+      //当前缩略图视图的列数
       coverColNum: 2,
+      //当前缩略图视图的行数
       row_conut: 1,
+      //当前列表的页码
       current_page: 1,
+      //当前book的总数
       total: 1,
       bookType: [
         '完结漫画', '连载漫画'
       ],
       fullScreenViewMode: false,
       drawer: false,
+      //全屏模式下的bookid
       CurrentBookId: 1,
+      //全屏模式下的页码数
       page: 1,
+      //全屏模式下book的长度
+      CurrentBookLength: 0,
     }
   },
   computed: {
@@ -151,13 +169,67 @@ export default {
       this.fullScreenViewMode = true
       this.CurrentBookId = id
       this.page = 1
-      console.log('fullScreenViewMode', this.fullScreenViewMode, this.CurrentBookId)
+      axios({
+        method: 'get',
+        url: `/comicManager/${id}/bookLength`,
+        baseURL: 'http://172.17.18.115:8089'
+      }).then(
+          (response) => {
+            console.log(response)
+            if (response.status === 200) {
+              console.log(response.data)
+              this.CurrentBookLength = response.data
+            }
+          }
+      ).catch(
+          (err) => {
+            console.log(err)
+          }
+      )
+      console.log('fullScreenViewMode', this.fullScreenViewMode, this.CurrentBookId, this.CurrentBookLength)
     },
     clcikExitButton() {
       this.fullScreenViewMode = false
     },
+    clcikImgLeft() {
+      console.log('clcikImgLeft')
+    },
+    clcikImgRight() {
+      console.log('clcikImgRight')
+    },
+    clcikDeleteButton(id) {
+      console.log('clcikDeleteButton', id)
+      axios({
+        method: 'delete',
+        url: `/comicManager/${id}/bookDelete`,
+        baseURL: 'http://172.17.18.115:8089'
+      }).then(
+          (response) => {
+            console.log(response)
+            if (response.status === 204) {
+              console.log(response.data)
+              this.getAllBooks()
+            }
+          }
+      ).catch(
+          (err) => {
+            console.log(err)
+          }
+      )
+    },
     clickNextPic() {
-      this.page += 1
+      if (this.page + 1 > this.CurrentBookLength) {
+        ElMessage({message: 'last page!', type: 'warning'})
+      } else {
+        this.page += 1
+      }
+    },
+    clickPreviousPic() {
+      if (this.page - 1 < 1) {
+        ElMessage({message: 'first page!', type: 'warning'})
+      } else {
+        this.page -= 1
+      }
     },
     getAllBooks() {
       axios({
@@ -171,8 +243,8 @@ export default {
             if (response.status === 200) {
               this.allBooksDataSource = response.data.results
               this.total = response.data.count
-              this.row_conut = Math.floor(this.allBooksDataSource.length / this.coverColNum)
-              console.log('this.row_conut, this.coverColNum', this.row_conut, this.coverColNum, this.allBooksDataSource, response.data)
+              this.row_conut = Math.ceil(this.allBooksDataSource.length / this.coverColNum)
+              console.log('getAllBooks', 'this.row_conut', this.row_conut, 'this.coverColNum', this.coverColNum, ' this.allBooksDataSource', this.allBooksDataSource)
               // this.pathCount = response.data.count
             }
           }
